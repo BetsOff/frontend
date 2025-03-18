9d6f391b7d68f233a18a7d37eefba43ceec5d26a
graua-git <graua@oregonstate.edu>
2025-03-18 11:18:53 -0400

    .

---
+++ app/(tabs)/standings.tsx
@@ -0,0 +1,8 @@
+import { ThemedText } from '@/components/general/ThemedText';
+import { StyleSheet } from 'react-native';
+
+export default function StandingsScreen() {
+  return (
+   <ThemedText>Standings</ThemedText>
+  );
+}
