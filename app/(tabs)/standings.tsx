d5027f4d56d141b210fc66adf7f2c11b4fe6dda4
graua-git <graua@oregonstate.edu>
2025-03-22 09:53:31 -0400

    fetching league, season, and matches

--- app/(tabs)/standings.tsx
+++ app/(tabs)/standings.tsx
@@ -8,11 +8,84 @@ import StandingsTable from '@/components/Standings/StandingsTable';
 import NoDataScreen from '../no_data';
 import CreateButton from '@/components/CreateButton';
 import { RootState } from '@/state/store';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { storageGetItem } from '@/util/Storage';
+import axios from 'axios';
+import apiRoutes from '@/routes/apiRoutes';
+import { setLeagues } from '@/state/LeagueSlice';
+import { setSeason } from '@/state/SeasonSlice';
+import { setMatches } from '@/state/MatchSlice';
 
 export default function StandingsScreen() {
+  const dispatch = useDispatch();
   const league = useSelector((state: RootState) => state.league.currentLeague);
   const season = useSelector((state: RootState) => state.season.season);
+
+  useEffect(() => {
+    const userId = storageGetItem('user_id');
+    const token = storageGetItem('token');
+    if (!userId || !token) return;
+  
+    const fetchLeagues = async () => {
+      console.log('fetching leagues')
+      if (!userId) return;
+      await axios.get(apiRoutes.league.get + `?user_id=${userId}`, {
+        headers: {
+          'X-Authorization': `Token ${token}`,
+        },
+      })
+      .then(response => {
+        dispatch(setLeagues(response.data));
+        const firstLeague = response.data.length > 0 
+          ? response.data[0] 
+          : null;
+        if (!firstLeague) return;
+        fetchSeason(firstLeague.id);
+      })
+      .catch(error => {
+        console.error('Error fetching league:', error);
+        return;
+      });
+    }
+
+    const fetchSeason = async (leagueId: number) => {
+      console.log('fetching season: league:', leagueId);
+      axios.get(apiRoutes.season.get + `?league_id=${leagueId}`, {
+        headers: {
+          'X-Authorization': `Token ${token}`,
+        },
+      })
+      .then(response => {
+        dispatch(setSeason(response.data));
+        const seasonId = response.data.id;
+        if (!seasonId) return;
+        fetchMatches(seasonId);
+      })
+      .catch(error => {
+        console.error('Error fetching season:', error);
+        return;
+      });
+    }
+    
+    const fetchMatches = async (seasonId: number) => {
+      console.log('fetching matches: season:', seasonId);
+      axios.get(apiRoutes.match.get + `?season_id=${seasonId}`, {
+        headers: {
+          'X-Authorization': `Token ${token}`,
+        },
+      })
+      .then(response => {
+        dispatch(setMatches(response.data));
+      })
+      .catch(error => {
+        console.error('Error fetching matches:', error);
+        return;
+      })
+    }
+
+    fetchLeagues();
+  }, []);
 
   if (!league) {
     return (
